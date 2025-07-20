d1ec28d372b5a0b3cfc5609e64f67087f9a2f966
try-again-later <aa.popok@yandex.ru>
2025-07-21 00:25:01 +0300

    Try out using github secrets to store API URL and key

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,16 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
 /// <reference types="vite-plugin-vue-layouts-next/client" />
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
